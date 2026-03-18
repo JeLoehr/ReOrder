88dc8141d3628e2639d10ac4c8a4119462c129f1
JeLoehr <50750837+JeLoehr@users.noreply.github.com>
2026-03-17 22:05:12 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,14 +1,6 @@
 const CACHE = 'reorder-v3';
-const FILES = [
-  '/reorder/',
-  '/reorder/index.html',
-  '/reorder/manifest.json',
-  '/reorder/icon-192.png',
-  '/reorder/icon-512.png'
-];
 
 self.addEventListener('install', function(e){
-  e.waitUntil(caches.open(CACHE).then(function(c){ return c.addAll(FILES); }));
   self.skipWaiting();
 });
 
@@ -20,9 +12,5 @@ self.addEventListener('activate', function(e){
 });
 
 self.addEventListener('fetch', function(e){
-  e.respondWith(
-    caches.match(e.request).then(function(cached){
-      return cached || fetch(e.request).catch(function(){ return caches.match('/reorder/'); });
-    })
-  );
+  e.respondWith(fetch(e.request).catch(function(){ return caches.match(e.request); }));
 });
